fb385cfcde1bef76407fcb47bbd705d1c76b5a06
poliik <43605977+poliik@users.noreply.github.com>
2022-02-26 17:39:47 +0300

    Update class2_features.py

--- functions/class2_features.py
+++ functions/class2_features.py
@@ -1,15 +1,5 @@
 import numpy as np
-from functions.geometry_module import symmetry_transform_up, orange_closer, is_boarder
-
-def classificate_class2(target):
-    if target >= 0.39 and target < 0.43:
-        return 1
-    if target >= 0.34 and target < 0.38:
-        return 2
-    if target >= 0.27 and target <= 0.31:
-        return 3
-    return 0
-
+from functions.geometry_module import symmetry_transform_up, orange_closer, is_boarder, dist_to_diag
 
 def unufication_class2(sample):
     sample['representative'] = sample['representative'].apply(lambda x: symmetry_transform_up(x.copy(), x[0].coords))
@@ -17,7 +7,20 @@ def unufication_class2(sample):
     
     return sample
 
+def distance_features(data):
+    data['dist_to_diag_Mo'] = data['representative'].apply(lambda x: dist_to_diag(x[0].coords))
+    data['dist_to_diag_S'] = data['representative'].apply(lambda x: dist_to_diag(x[1].coords))
+    data['dist_to_diag_Se'] = data['representative'].apply(lambda x: dist_to_diag(x[2].coords))
+    
+    data['Mo_S_dist'] = data['representative'].apply(lambda x: np.linalg.norm(x[0].coords - x[1].coords)**0.5)
+    data['S_Se_dist'] = data['representative'].apply(lambda x: np.linalg.norm(x[1].coords - x[2].coords)**0.5)
+    data['Mo_Se_dist'] = data['representative'].apply(lambda x: np.linalg.norm(x[0].coords - x[2].coords)**0.5)
+    return data
+
+
 def geometrical_features(data):
+    data = distance_features(data)
+
     for i in range(3):
         data['Mo_coords_' + str(i)] = data['representative'].apply(lambda x: x[0].frac_coords[i])
 
@@ -26,11 +29,6 @@ def geometrical_features(data):
 
     for i in range(3):
         data['Se_coords_' + str(i)] = data['representative'].apply(lambda x: x[2].frac_coords[i])
-
-    data['Mo_S_dist'] = data['representative'].apply(lambda x: np.linalg.norm(x[0].coords - x[1].coords)**0.5)
-    data['S_Se_dist'] = data['representative'].apply(lambda x: np.linalg.norm(x[1].coords - x[2].coords)**0.5)
-    data['Mo_Se_dist'] = data['representative'].apply(lambda x: np.linalg.norm(x[0].coords - x[2].coords)**0.5)
-
 
     data['M_is_boarder'] = data['representative'].apply(lambda x: is_boarder(x[0]))
     data['S_is_boarder'] = data['representative'].apply(lambda x: is_boarder(x[1]))
